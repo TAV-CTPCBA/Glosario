95c92707c3b091a9c728566046595ac408d00077
Marian <marianojacobo@gmail.com>
2018-07-11 21:01:28 -0300

    cambio de estructura

--- glosario.js
+++ glosario.js
@@ -1,8 +1,16 @@
-const endpoint = 'https://gist.githubusercontent.com/marianjay/86027d7d96a06b8fc8604436ceeb7eea/raw/49efc5a05184a2ecaed6c32c06a4c8eb33612704/palabras.json';
-const palabras = [];
-fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => palabras.push(...data));
+const searchInput = document.querySelector('.search');
+const results = document.getElementById('results');
+var html;
+
+
+function populate() {
+  html = `
+  <div class="inner-wrapper">
+    ${palabras.map(palabra => `<li>${palabra.ingles} - ${palabra.español}</li>`).join('')}
+  </div>
+`;
+results.innerHTML = html;
+}
 
 
 function findMatches(wordToMatch, palabras) {
@@ -13,29 +21,25 @@ function findMatches(wordToMatch, palabras) {
   });
 }
 
+
 function displayMatches() {
   const matchArray = findMatches(this.value, palabras);
-  if(this.value.length >= 3) {
-    const html = matchArray.map(palabra => {
-      return `
-        <div class="inner-wrapper">
-          <p>${palabra.ingles}</p>
-          <p>${palabra.español}</p>
-        </div>
-      `;
-    }).join('');
-    results.innerHTML = html;
+    if(this.value.length >= 3 ) {
+      const html = matchArray.map(palabra => {
+        return `
+          <div class="inner-wrapper">
+            <li>${palabra.ingles} - ${palabra.español}</li>
+          </div>
+        `;
+      }).join('');
+      results.innerHTML = html;
   }
-}
+  if(this.value.length < 3) {
+    populate();
+  }
+};
 
-function checkInput() {
-  if(searchInput.value === '') {
-    results.innerHTML = '';
-  }
-}
-const searchInput = document.querySelector('.search');
-const results = document.getElementById('results');
 searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('change', checkInput);
 searchInput.addEventListener('keyup', displayMatches);
-searchInput.addEventListener('keyup', checkInput);
+window.addEventListener('load', populate);
+
