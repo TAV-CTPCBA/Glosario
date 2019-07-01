11c09d4db0aa27ca3cd011bb4770b14680da3d6f
Wadi <marianojacobo@gmail.com>
2019-07-01 15:51:09 -0300

    Cambios

--- glosario.js
+++ glosario.js
@@ -1,45 +1,58 @@
-const searchInput = document.querySelector('.search');
-const results = document.getElementById('results');
-var html;
+  const searchInput = document.querySelector('.search');
+  const results = document.getElementById('results');
+  var html;
 
 
-function populate() {
-  html = `
-  <div class="inner-wrapper">
-    ${palabras.map(palabra => `<li>${palabra.ingles} - ${palabra.español}</li>`).join('')}
-  </div>
+  function populate() {
+    html = `
+      <tr class="header">
+        <th>Inglés</th>
+        <th>Neutro</th>
+        <th>Variaciones</th>
+      </tr>
+        ${palabras.map(palabra => `
+        <tr>
+        <td>${palabra.ingles}</td>
+        <td>${palabra.español}</td>
+        <td>${palabra.otras}</td>
+        </tr>`).join('')}
 `;
-results.innerHTML = html;
-}
-
-
-function findMatches(wordToMatch, palabras) {
-  return palabras.filter(palabra => {
-    // here we need to figure out if the city or state matches what was searched
-    const regex = new RegExp(wordToMatch, 'gi');
-    return palabra.ingles.match(regex)
-  });
-}
-
-
-function displayMatches() {
-  const matchArray = findMatches(this.value, palabras);
-    if(this.value.length >= 3 ) {
-      const html = matchArray.map(palabra => {
-        return `
-          <div class="inner-wrapper">
-            <li>${palabra.ingles} - ${palabra.español}</li>
-          </div>
-        `;
-      }).join('');
-      results.innerHTML = html;
+  results.innerHTML = html;
   }
-  if(this.value.length < 3) {
-    populate();
-  }
-};
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
-window.addEventListener('load', populate);
+
+  function findMatches(wordToMatch, palabras) {
+    return palabras.filter(palabra => {
+      // here we need to figure out if the city or state matches what was searched
+      const regex = new RegExp(wordToMatch, 'gi');
+      return palabra.ingles.match(regex) || palabra.español.match(regex) || palabra.otras.match(regex)
+    });
+  }
+
+  function displayMatches() {
+    const matchArray = findMatches(this.value, palabras);
+      if(this.value.length >= 1 && matchArray.length >= 1) {
+        const html = `          <tr class="header">
+        <th>Inglés</th>
+        <th>Neutro</th>
+        <th>Variaciones</th>
+      </tr> ${matchArray.map(palabra => {
+          return `
+          <tr>
+            <td> ${palabra.ingles} </td>
+            <td> ${palabra.español} </td>
+            <td> ${palabra.otras}</td>
+          </tr>
+          `;
+        }).join('')}`;
+        results.innerHTML = html;
+    }
+    if(this.value.length < 1) {
+      populate();
+    }
+  };
+
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', displayMatches);
+  window.addEventListener('load', populate);
 
